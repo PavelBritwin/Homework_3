ef7c1be86616d5a0865fcca5303065115e07a56a
Pavel Anokhin <anohinpa@gmail.com>
2022-09-23 21:46:00 +0300

    Task 19

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 19
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+string inputNumberStr = inputNumber.ToString();
+if (inputNumberStr[0] == inputNumberStr[4])
+{
+    if (inputNumberStr[1] == inputNumberStr[3]) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
+else Console.WriteLine("нет");
