30e9eb4505326006ce2d5b3ceab310a314d3017d
Pavel Anokhin <anohinpa@gmail.com>
2022-09-23 21:55:31 +0300

    Task 21

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿// Task 19
+﻿// Task 21
 Console.Clear();
-Console.WriteLine("Введите число: ");
-int inputNumber = Convert.ToInt32(Console.ReadLine());
-string inputNumberStr = inputNumber.ToString();
-if (inputNumberStr[0] == inputNumberStr[4])
-{
-    if (inputNumberStr[1] == inputNumberStr[3]) Console.WriteLine("да");
-    else Console.WriteLine("нет");
-}
-else Console.WriteLine("нет");
+Console.WriteLine("Введите координаты первой точки: ");
+int[] firstPoint = new int[]{Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
+Console.WriteLine("Введите координаты второй точки: ");
+int[] secondPoint = new int[]{Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
+
+//√((x2-x1)^2 )+(y2-y1)^2+(z2-z1)^2
+double firstOp = Math.Pow(secondPoint[0] - firstPoint[0], 2);
+double secondOp = Math.Pow(secondPoint[1] - firstPoint[1], 2);
+double thirdOp = Math.Pow(secondPoint[2] - firstPoint[2], 2);
+double result = Math.Sqrt(firstOp  + secondOp + thirdOp);
+
+Console.WriteLine(result);
