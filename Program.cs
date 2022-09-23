28b97e0dc4c830b68c646e11f1ef0c4fff57877c
Pavel Anokhin <anohinpa@gmail.com>
2022-09-23 21:58:21 +0300

    Task 23

--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
-﻿// Task 21
+﻿// Task 23
 Console.Clear();
-Console.WriteLine("Введите координаты первой точки: ");
-int[] firstPoint = new int[]{Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
-Console.WriteLine("Введите координаты второй точки: ");
-int[] secondPoint = new int[]{Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
-
-//√((x2-x1)^2 )+(y2-y1)^2+(z2-z1)^2
-double firstOp = Math.Pow(secondPoint[0] - firstPoint[0], 2);
-double secondOp = Math.Pow(secondPoint[1] - firstPoint[1], 2);
-double thirdOp = Math.Pow(secondPoint[2] - firstPoint[2], 2);
-double result = Math.Sqrt(firstOp  + secondOp + thirdOp);
-
-Console.WriteLine(result);
+Console.WriteLine("Введите N: ");
+double n = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+while (i <= n)
+{
+    if (i != n) Console.Write($"{Math.Pow((i),3)}, ");
+    else Console.Write($"{Math.Pow((i),3)}.");
+    i++;
+}
